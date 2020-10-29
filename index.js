f9ccff310cb123cd1affecb6fdd9bebea17b4d53
Samtou <christophe1999da@gmail.com>
2020-10-29 23:40:28 +0000

    bodi parser

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
 const Thing = require('./models/thing');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const accountSid = process.env.TWILIO_ACCOUNT_SID ;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+console.log(accountSid, authToken);
+
+try {
+	const client = require('twilio')(accountSid, authToken);
+}
+catch (e) {
+	console.log('ENV VAR NOT FUND');
+}
 
 const PORT = process.env.PORT || 5000;
+
+const jsonParser = bodyParser.json();
 
 
 mongoose.connect('mongodb+srv://admin:admin@cluster0.eal7i.mongodb.net/doctor_sex?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -18,7 +32,7 @@ express()
 	
 	.get('/', (req, res) => res.render('pages/index'))
 
-	.post('/wa', (req, res) => {
+	.post('/wa', jsonParser, (req, res) => {
 		console.log(req.body);
 
 		const message = "cc";
@@ -31,3 +45,20 @@ express()
 	})
 	
 	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+
+
+
+
+
+
+
+// client.messages
+//   .create({
+//      mediaUrl: ['https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'],
+//      from: 'whatsapp:+14155238886',
+//      body: `It's taco time!`,
+//      to: 'whatsapp:+15017122661'
+//    })
+//   .then(message => console.log(message.sid));
